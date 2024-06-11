85e0037edff24d5848567f0cdf71deaedce2d0b2
Kshitij Anand <kshitijkumar2002@gmail.com>
2024-06-11 12:18:53 +0530

    feat/Profilepic

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,39 @@
 import React from "react";
-import profilePic from "../assets/kevinRushProfile.png"
+import profilePic from "../assets/profilePic.png"
 import { HERO_CONTENT } from "../constants";
+import { motion } from "framer-motion";
+const container=(delay)=>({
+    hidden:{x:-100,opacity:0},
+    visible:{
+        x:0,
+        opacity:1,
+        transition:{duration:0.5,delay:delay}
+    }
+})
+    
+
 const Hero = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg-mt-16
+            <motion.h1
+            variants={container(0)}
+            initial:hidden
+             className="pb-16 text-6xl font-thin tracking-tight lg-mt-16
             lg-text-8xl">
-                Kshitij Anand</h1>
-                <span className="bg-gradient-to-r from-pink-300 via-slate-500 
-                to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer</span>
+                Kshitij Anand</motion.h1>
+                <motion.span 
+                
+                className="bg-gradient-to-r from-pink-300 via-slate-500 
+                to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">Full Stack Developer</motion.span>
                 <p className="my-2 mx-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</p>
+                <a href="path_to_your_resume.pdf" download="Resume.pdf">
+                  <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Resume
+                  </button>
+                </a>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -9,7 +9,7 @@ const Technologies = () => {
     <div className="border-b border-neutral-800 pb-24">
       <h2 className="my-20 text-center text-4xl">
         Technologies
-        <div className="flex flex-wrap items-center justify-center gap-4">
+        <div className="flex flex-wrap items-center justify-center gap-4 pt-12">
           <div className="rounded-2xl border-4 border-neutral-800 p-4">
             <RiReactjsLine className="text-7xl text-cyan-400"/>
 
