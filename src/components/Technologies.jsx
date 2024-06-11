0f8381ca8417ac68a95818e56711bc3df6c95bc0
Kshitij Anand <kshitijkumar2002@gmail.com>
2024-06-11 14:42:43 +0530

    feat/animation in techno icons div

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -22,9 +22,18 @@ const iconVariants=(duration)=>({
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <h2 className="my-20 text-center text-4xl">
+      <motion.h2 
+       whileInView={{opacity:1,y:0}}
+       initial={{opacity:0,y:-100}}
+       transition={{duration:1.5}}
+
+      className="my-20 text-center text-4xl">
         Technologies
-        <div 
+        <motion.div 
+         whileInView={{opacity:1,x:0}}
+         initial={{opacity:0,x:-100}}
+         transition={{duration:1.5}}
+
        
         className="flex flex-wrap items-center justify-center gap-4 pt-12">
           <motion.div 
@@ -72,8 +81,8 @@ const Technologies = () => {
             <FaNodeJs className="text-7xl text-green-500"/>
 
           </motion.div>
-        </div>
-      </h2>
+        </motion.div>
+      </motion.h2>
     </div>
   );
 };
